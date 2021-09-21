dfd4ec32d4ba133f6ab01e0021690955daa9f7c5
scy <13057160969@163.com>
2021-09-21 17:34:04 +0800

    新增数据成功

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,14 @@ class Users {
 
   // 新增用户
   async createUser(ctx) {
-    ctx.body = '114'
+    // koa-bodyparser 中间件，使用 `ctx.request.body` 进行获取 POST 请求参数
+    const postData = ctx.request.body
+    // 往数据库插入数据
+    let sql = "INSERT INTO userInfo" // userInfo为表名
+    sql += ` (id, name) VALUES ('${postData.id}','${postData.name}' )`
+    dataBase.query(sql)
+    // 将post传的参数返回客户端
+    ctx.body = postData
   }
 }
 
